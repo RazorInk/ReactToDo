09c29393efb4f3dac4803b5aa410878d1962a912
RazorInk <stephenseymour98@gmail.com>
2020-05-02 12:16:13 -0400

    Basic Setup

--- App.js
+++ App.js
@@ -1,73 +1,11 @@
-import React, { useState } from 'react';
-import { Text, View,StyleSheet,FlatList, Alert,
-  TouchableWithoutFeedback, Keyboard } from 'react-native';
-import Header from './components/header';
-import Product from './components/product';
-import AddProduct from './components/addProduct';
+import React from 'react';
+import Home from './screens/home';
 
 export default function App() {
-
-  const [prods,setProds] = useState([
-    {text:'Bananas', key: 1},
-    {text: 'Casavas', key:2},
-    {text: 'Limes', key:3}
-  ]);
-
-  const pressHandler = (key) => {
-    setProds(prevProds => {
-      return prevProds.filter(prod=> prod.key!=key);
-    });
-  };
-
-  const submitHandler = (text) => {
-    if (text.length > 3){
-      setProds(prevProds =>{
-        return[
-          {text,key: Math.random().toString() },
-          ...prevProds
-        ];
-      });
-    } else {
-      Alert.alert('Short entry', 'Product name must be at least 3 characters long',
-        [ {text:'OK',onPress:() => console.log('alert closed') }
-      ]);
-    }
-  };
-
   return (
-    <TouchableWithoutFeedback onPress={()=>{
-      Keyboard.dismiss();
-      console.log('dismissed keyboard');
-    }}>
-      <View style = {styles.container}>
-        <Header />
-        <View style = {styles.content}>
-          <AddProduct submitHandler={submitHandler}/>
-          <View style = {styles.list}>
-              <FlatList 
-              data={prods} 
-              renderItem={ ({item}) =>(
-                <Product item = {item} pressHandler={pressHandler} />
-              )}
-              />
-            </View>
-          </View>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+    <Home />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  content: {
-    flex:1,
-    padding: 40,
-  },
-  list: {
-    flex: 1,
-    marginTop: 20,
-  }
-});
+
+
